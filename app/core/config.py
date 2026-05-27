7f0c4937465ace8be774300ba01c83627cf609e0
yrlmanoharreddy <yrlmanoharreddymeda@gmail.com>
2026-05-27 11:43:08 -0500

    Initial structure code

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "LLM Provider Gateway"
+    app_env: str = "local"
+
+    groq_api_key: str | None = None
+    gemini_api_key: str | None = None
+
+    ollama_base_url: str = "http://localhost:11434"
+    ollama_model: str = "llama3.2:1b"
+
+    groq_base_url: str = "https://api.groq.com/openai/v1"
+    groq_model: str = "openai/gpt-oss-20b"
+
+    class Config:
+        env_file = ".env"
+        extra = "ignore"
+
+Settings = Settings()
---
+++ app/providers/base.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from app.schemas.llm_schema import LLMRequest
+
+class BaseLLMProvider(ABC):
+
+    @abstractmethod
+    def generate(self, request: LLMRequest)-> tuple[str, str]
+        """
+        Returns:
+        output_text, model_name
+        """
+        pass
